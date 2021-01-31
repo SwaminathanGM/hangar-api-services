5cbc670dd13af21efdddc0b6cf00e73be843ffed
Aravind Venkatesan <aravindtherocker@gmail.com>
2021-01-31 18:33:24 +0800

    Initial Commit

---
+++ src/main/java/com/hangar/hangarapiservices/HangarApiServicesApplication.java
@@ -0,0 +1,13 @@
+package com.hangar.hangarapiservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HangarApiServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HangarApiServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hangar/hangarapiservices/HangarApiServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hangar.hangarapiservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HangarApiServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
